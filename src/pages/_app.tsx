e4f5583df0b21d68abd901ffd0b63f26cd8798fe
mishina naoya <msnyxx811@gmail.com>
2024-01-14 21:39:17 +0900

    chakraUI導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+
+import { fonts } from '../lib/fonts'
  
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -14,5 +17,18 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page)
  
-  return getLayout(<Component {...pageProps} />)
+  return getLayout(
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --font-rubik: ${fonts.rubik.style.fontFamily};
+          }
+        `}
+      </style>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  )
 }
